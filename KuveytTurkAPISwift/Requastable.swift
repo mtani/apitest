498d09cd241bd76b445e16062a1bf3fb363e4249
Mustafa Tanışır / Kuveyt Türk - BT-Dijital Bankacılık Yazılım Geliştirme <mustafa.tanisir@kuveytturk.com.tr>
2018-01-08 08:56:31 +0300

    add

---
+++ KuveytTurkAPISwift/Requastable.swift
@@ -0,0 +1,23 @@
+//
+//  Requastable.swift
+//  KuveytTurkAPISwift
+//
+//  Created by Mustafa Tanışır / Kuveyt Türk - BT-Dijital Bankacılık Yazılım Geliştirme on 26.12.2017.
+//  Copyright © 2017 Kuveyt Türk. All rights reserved.
+//
+
+import UIKit
+import p2_OAuth2
+
+
+public typealias OAuth2JSON =  [String: Any]
+
+public typealias OAuthStringDict = [String: String]
+
+public typealias OAuthHeaders = [String: String]
+
+
+open class Requestable: OAuth2Requestable {
+
+}
+
